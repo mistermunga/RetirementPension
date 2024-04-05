e0a906a4d90b948f01d814dfa47beeb1696de6d3
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2024-04-05 18:14:59 +0300

    hourly employee table & form

--- src/retiremntpensions/AppendSalariedEmployee.java
+++ src/retiremntpensions/AppendSalariedEmployee.java
@@ -11,12 +11,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author Ryan
  */
-public class append extends javax.swing.JFrame {
+public class AppendSalariedEmployee extends javax.swing.JFrame {
 
     /**
      * Creates new form append
      */
-    public append() {
+    public AppendSalariedEmployee() {
         initComponents();
     }
 
@@ -200,20 +200,21 @@ public class append extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(append.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppendSalariedEmployee.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(append.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppendSalariedEmployee.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(append.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppendSalariedEmployee.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(append.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppendSalariedEmployee.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new append().setVisible(true);
+                new AppendSalariedEmployee().setVisible(true);
             }
         });
     }
